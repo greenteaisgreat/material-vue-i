c3887b0fb73c81b6be34cb7f5d4a96625e5e0725
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-06-02 18:40:17 -0500

    Add components directory & <AppNavLink />

---
+++ src/components/AppNavLink.vue
@@ -0,0 +1,52 @@
+<script setup lang='ts'>
+import {computed, ref} from 'vue'
+
+interface DropdownItem {
+  label: string
+  href: string
+}
+
+interface Props {
+  label: string
+  href?: string
+  dropdown?: boolean
+  items?: DropdownItem[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  href: '#',
+  dropdown: false,
+  items: () => []
+})
+
+// boolean that checks if AppNavLink has been hovered over
+const isOpen = ref(false)
+
+// checks for any dropdown items
+const hasDropdown = computed(
+  () => props.dropdown && props.items.length > 0
+)
+
+function openMenu() {
+  if (hasDropdown.value) {
+    isOpen.value = true;
+  }
+}
+
+function closeMenu() {
+  isOpen.value = false
+}
+</script>
+
+<template>
+  <div
+    class='nav-link'
+    @mouseenter='openMenu'
+    @mouseleave='closeMenu'
+  >
+  </div>
+</template>
+
+<style scoped>
+
+</style>

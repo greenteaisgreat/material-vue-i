14c21c84f8f23895fa4d8f65ad3bb75bb4e04b2f
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-06-02 18:40:37 -0500

    Import <AppNavLink /> to App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import AppNavLink from './components/AppNavLink.vue'
+</script>
 
 <template>
+  <AppNavLink />
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the

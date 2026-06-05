52eeb29a533634294483523168ecce9cc491ca8b
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-06-04 19:24:34 -0500

    Add <AppHeader /> component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import AppNavLink from './AppNavLink.vue'
+
+interface NavItem {
+  label: string
+  href?: string
+  dropdown?: boolean
+  items?: {
+    label: string
+    href: string
+  }[]
+}
+
+defineProps<{
+  links: NavItem[]
+}>()
+</script>
+
+<template>
+  <header class="header">
+    <nav class="header__nav">
+      <AppNavLink
+        v-for="link in links"
+        :key="link.label"
+        :label="link.label"
+        :href="link.href"
+        :dropdown="link.dropdown"
+        :items="link.items"
+      />
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>

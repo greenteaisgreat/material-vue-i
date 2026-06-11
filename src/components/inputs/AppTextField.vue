d8be37d2704712cc42c8d94335119e70e51a852d
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-06-11 17:33:06 -0500

    Add <AppTextField /> logic. TODO: styling

---
+++ src/components/inputs/AppTextField.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface Props {
+  label?: string
+  modelValue: string
+  placeholder?: string
+}
+
+const { label, modelValue, placeholder = '' } = defineProps<Props>()
+
+const emit = defineEmits<{
+  'update: modelValue': [value: string]
+}>()
+
+function handleInput(event: Event) {
+  emit('update: modelValue', (event.target as HTMLInputElement).value)
+}
+</script>
+
+<template>
+  <label class="field">
+    <span v-if="label">
+      {{ label }}
+    </span>
+    <input :value="modelValue" :placeholder="placeholder" @input="handleInput" />
+  </label>
+</template>
+
+<style scoped></style>
